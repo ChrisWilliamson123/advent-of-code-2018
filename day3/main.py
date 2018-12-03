b8dee623d0d22c7f13f5957decede6972151d1d2
Chris Williamson <chris.williamson@bbc.co.uk>
2018-12-03 14:16:28 +0000

    Completed day 3

--- day3/main.py
+++ day3/main.py
@@ -1,5 +1,36 @@
+from collections import Counter
+
+def parse_claim(claim):
+  split = claim.split(' ')
+  id = int(split[0].replace('#', ''))
+  origin = tuple([int(x) for x in split[2].replace(':', '').split(',')])
+  area = tuple([int(x) for x in split[-1].split('x')])
+  return (id, origin, area)
+
 def main():
-  print("Hello World!")
+  fabric_input = [l.rstrip() for l in open('input.txt', 'r').readlines()]
+  coords_to_ids = {}
+  all_ids = set()
+  dupe_ids = set()
+  dupe_coords = set()
+
+  for c in fabric_input:
+    id, origin, area = parse_claim(c)
+    all_ids.add(id)
+    for x in range(origin[0], origin[0]+area[0]):
+      for y in range(origin[1], origin[1]+area[1]):
+        coord = (x, y)
+        if coord in coords_to_ids:
+          coords_to_ids[coord].append(id)
+          for dupe_id in coords_to_ids[coord]:
+            dupe_ids.add(dupe_id)
+          dupe_coords.add(coord)
+        else:
+          coords_to_ids[coord] = [id]
+
+  print(len(dupe_coords))
+  print(all_ids - dupe_ids)
+
 
 if __name__ == '__main__':
   main()
