b69b2052d19b4189255e6ef5d67c329afba371a5
Chris Williamson <chris.williamson@bbc.co.uk>
2018-12-05 09:42:15 +0000

    Completed part1 of day 5

--- day5/main.py
+++ day5/main.py
@@ -1,5 +1,19 @@
+remove_units_from_polymer = lambda p, i: p[:i] + p[i+2:]
+
 def main():
-  print("Hello World!")
+  polymer = open('input.txt', 'r').readline()
+  index = 0
+  while index < len(polymer)-1:
+    current_unit = polymer[index]
+    next_unit = polymer[index+1]
+
+    if next_unit.swapcase() == current_unit:
+      polymer = remove_units_from_polymer(polymer, index)
+      index = index - 1 if index != 0 else 0
+    else:
+      index += 1
+
+  print(len(polymer))
 
 if __name__ == '__main__':
   main()
