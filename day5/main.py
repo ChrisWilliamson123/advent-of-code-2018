f83f1d68b6c264fab7727a29e9c406b829bbc97c
Chris Williamson <chris.williamson@bbc.co.uk>
2018-12-05 09:56:16 +0000

    Completed part 2.

--- day5/main.py
+++ day5/main.py
@@ -1,7 +1,8 @@
+import operator
+
 remove_units_from_polymer = lambda p, i: p[:i] + p[i+2:]
 
-def main():
-  polymer = open('input.txt', 'r').readline()
+def react_polymer(polymer):
   index = 0
   while index < len(polymer)-1:
     current_unit = polymer[index]
@@ -13,7 +14,20 @@ def main():
     else:
       index += 1
 
-  print(len(polymer))
+  return polymer
+
+def main():
+  polymer = open('input.txt', 'r').readline()
+  
+  letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  letters_to_poly_length = {}
+  
+  for l in letters:
+    cleansed_polymer = polymer.replace(l, '').replace(l.upper(), '')
+    reacted_polymer = react_polymer(cleansed_polymer)
+    letters_to_poly_length[l] = len(reacted_polymer)
+  
+  print(min(letters_to_poly_length.values()))
 
 if __name__ == '__main__':
   main()
