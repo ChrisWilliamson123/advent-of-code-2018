395d1ba13d7cb28a57a26d1cbcb094fe5e4bb927
Chris Williamson <chris.williamson@bbc.co.uk>
2018-12-14 09:46:36 +0000

    Optimised day 1.

--- day1/part2.py
+++ day1/part2.py
@@ -1,16 +1,22 @@
+from collections import defaultdict
+
 def main():
   frequencies = [int(f) for f in open('input.txt', 'r').readlines()]
-  current_freq = 0
-  results = [0]
-  freq_index = 0
+  frequencies_reached = defaultdict(int)
+  frequencies_reached[0] = 1
+  latest_f = 0
 
-  while True:
-    current_freq += frequencies[freq_index] 
-    if current_freq in results:
-      print(current_freq)
-      break
-    results.append(current_freq)
-    freq_index = freq_index + 1 if freq_index < len(frequencies) - 1 else 0
+  found = False
+  f_index = 0
+  while not found:
+    next_f = latest_f + frequencies[f_index]
+    if frequencies_reached[next_f]:
+      found = True
+      print(next_f)
+    else:
+      latest_f = next_f
+      frequencies_reached[next_f] = 1
+      f_index = f_index + 1 if f_index + 1 < len(frequencies) else 0
 
 if __name__ == '__main__':
   main()
