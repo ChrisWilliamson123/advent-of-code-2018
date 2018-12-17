659ca504bca1ec65860d0a7c586703fe622e1c2a
Chris Williamson <chris.williamson@bbc.co.uk>
2018-12-17 17:51:16 +0000

    Completed day 17

--- day15/main.py
+++ day15/main.py
@@ -1,5 +1,210 @@
+import operator
+from collections import defaultdict
+
+def parse_cave(cave_text):
+  cave = defaultdict(tuple)
+  elves = defaultdict(tuple)
+  goblins = defaultdict(tuple)
+  for y, l in enumerate(cave_text):
+    for x, c in enumerate(l):
+      coord = (x, y)
+      if c != '#':
+        cave[coord] = c
+      if c == 'E':
+        elves[coord] = {
+          'hp': 200
+        }
+      elif c == 'G':
+        goblins[coord] = {
+          'hp': 200
+        }
+  return cave, elves, goblins
+
+def get_adjacent_squares(p):
+  return [
+    (p[0], p[1]-1),
+    (p[0], p[1]+1),
+    (p[0]-1, p[1]),
+    (p[0]+1, p[1])
+  ]
+
+def get_open_squares(target, cave):
+  open_squares = []
+  to_check = get_adjacent_squares(target)
+  for c in to_check:
+    if cave[c] == '.':
+      open_squares.append(c)
+  return open_squares
+
+def get_close_enemies(targets, adjacent_squares):
+  close_enemies = []
+  for t in targets:
+    if t in adjacent_squares:
+      close_enemies.append(t)
+  return close_enemies
+
+def get_shortest_distance(origin, target, cave, distance, visited):
+  visited.append(origin)
+  adjacent_squares = get_adjacent_squares(origin)
+  if target in adjacent_squares:
+    return distance, origin
+  
+  open_squares = []
+  for c in adjacent_squares:
+    if cave[c] == '.' and c not in visited:
+      open_squares.append(c)
+  if not open_squares:
+    return 10000, (1000, 1000)
+
+  distances = []
+  visited_len = len(visited)
+  for c in open_squares:
+    # if c == (4, 5):
+    #   print('here')
+    shortest_distance, coord = get_shortest_distance(c, target, cave, distance+1, visited)
+    distances.append((shortest_distance, coord))
+    visited = visited[0:visited_len+1]
+  sorted_distances = sorted(distances, key=lambda d: (d[0], d[1][1], d[1][0]))
+  minimum = sorted_distances[0]
+  return minimum
+
 def main():
-  print("Hello World!")
+  attack_power = 3
+  hit_points = 200
+  cave_text = [l.rstrip() for l in open('input.txt', 'r').readlines()]
+  cave, elves, goblins = parse_cave(cave_text)
+  print('Starting elves: {0}'.format(elves))
+  print('Starting goblins: {0}'.format(goblins))
+
+  elf_coords = list(elves.keys())
+  goblin_coords = list(goblins.keys())
+
+  player_positions = sorted(
+    elf_coords + goblin_coords,
+    key=lambda x: (x[1], x[0])
+  )
+
+  end = False
+  rounds = 0
+
+  while not end:
+    index = 0
+    while index < len(player_positions):
+      pos = player_positions[index]
+      adjacent_squares = get_adjacent_squares(pos)
+      is_elf = pos in elves
+      if is_elf:
+        enemies = goblins
+        possible_targets = goblin_coords
+      else:
+        enemies = elves
+        possible_targets = elf_coords
+
+      if len(possible_targets) == 0:
+        print("No more targets!")
+        end = True
+        elves_win = is_elf
+        break
+      
+      close_enemies = get_close_enemies(possible_targets, adjacent_squares)
+      if close_enemies:
+        # print("Enemy(s) close")
+        # print(close_enemies, enemies)
+        sorted_by_health = sorted(close_enemies, key=lambda x: (enemies[x]['hp'], x[1], x[0]))
+        enemy_to_attack = sorted_by_health[0]
+        enemies[enemy_to_attack]['hp'] -= attack_power
+        if enemies[enemy_to_attack]['hp'] <= 0:
+          cave[enemy_to_attack] = '.'
+          del enemies[enemy_to_attack]
+          if is_elf:
+            # print("Goblin at {0} dead".format(enemy_to_attack))
+            goblin_coords = list(goblins.keys())
+          else:
+            # print("Elf at {0} dead".format(enemy_to_attack))
+            elf_coords = list(elves.keys())
+          player_pos_index = player_positions.index(enemy_to_attack)
+          player_positions.pop(player_pos_index)
+          if player_pos_index > index:
+            index += 1
+        else:
+          index += 1
+        continue
+
+      open_squares_around_targets = []
+      for t in possible_targets:
+        open_squares_around_targets += get_open_squares(t, cave)
+      if not open_squares_around_targets:
+        # print('No possible squares around targets.')
+        index += 1
+        continue
+
+      shortest_distances = []
+      for c in open_squares_around_targets:
+        print('Working out distance between {0} and {1}'.format(c, pos))
+        result = get_shortest_distance(c, pos, cave, 0, [])
+        shortest_distances.append(result)
+      to_move_to = sorted(shortest_distances, key=lambda d: (d[0], d[1][1], d[1][0]))[0][1]
+      if to_move_to != (1000, 1000):
+        if is_elf:
+          current = elves[pos]
+          cave[pos] = '.'
+          elves[to_move_to] = current
+          cave[to_move_to] = 'E'
+          del elves[pos]
+          elf_coords = list(elves.keys())
+        else:
+          current = goblins[pos]
+          cave[pos] = '.'
+          goblins[to_move_to] = current
+          cave[to_move_to] = 'G'
+          del goblins[pos]
+          goblin_coords = list(goblins.keys())
+        player_positions[index] = to_move_to
+
+      adjacent_squares = get_adjacent_squares(player_positions[index])
+      close_enemies = get_close_enemies(possible_targets, adjacent_squares)
+      if close_enemies:
+        # print("Enemy(s) close")
+        # print(close_enemies, enemies)
+        sorted_by_health = sorted(close_enemies, key=lambda x: (enemies[x]['hp'], x[1], x[0]))
+        enemy_to_attack = sorted_by_health[0]
+        enemies[enemy_to_attack]['hp'] -= attack_power
+        if enemies[enemy_to_attack]['hp'] <= 0:
+          cave[enemy_to_attack] = '.'
+          del enemies[enemy_to_attack]
+          if is_elf:
+            # print("Goblin at {0} dead".format(enemy_to_attack))
+            goblin_coords = list(goblins.keys())
+          else:
+            # print("Elf at {0} dead".format(enemy_to_attack))
+            elf_coords = list(elves.keys())
+          player_pos_index = player_positions.index(enemy_to_attack)
+          player_positions.pop(player_pos_index)
+          if player_pos_index > index:
+            index += 1
+        else:
+          index += 1
+        continue
+
+      index += 1
+    # print('\n')
+    rounds += 1
+    print(rounds)
+    player_positions = sorted(player_positions, key=lambda x: (x[1], x[0]))
+    # print(rounds)
+    # for coord, data in goblins.items():
+    #   print(coord, data['hp'])
+    # for coord, data in elves.items():
+    #   print(coord, data['hp'])
+  print(goblins)
+  print(elves)
+  print(rounds)
+
+
+  
+  # for coord in sorted(cave.keys(), key=lambda c: (c[1], c[0])):
+    
+
 
 if __name__ == '__main__':
   main()
--- day17/main.py
+++ day17/main.py
@@ -1,5 +1,127 @@
+import re
+import operator
+from collections import defaultdict, namedtuple
+
+class Point(namedtuple('Point', 'x y')):
+  def __add__(self, other):
+    return Point(self.x + other.x, self.y + other.y)
+
+SPRING = Point(500, 0)
+UP = Point(0, -1)
+DOWN = Point(0, 1)
+LEFT = Point(-1, 0)
+RIGHT = Point(1, 0)
+
+def parse_clay(clay):
+  positions = defaultdict(int)
+  for entry in clay:
+    single_coord_index = 0 if entry[0] == 'x' else 1
+    single, range_start, range_end = [int(n) for n in re.findall('\d+', entry)]
+    for c in range(range_start, range_end+1):
+      coord = [0, 0]
+      coord[single_coord_index] = single
+      coord[int(not single_coord_index)] = c
+      positions[tuple(coord)] = 1
+  return positions
+
+def is_blocked_left(origin, clay_positions, settled_positions):
+  one_left = (origin[0]-1, origin[1])
+  one_left_below = (one_left[0], one_left[1]+1)
+  while clay_positions[one_left_below] or settled_positions[one_left_below]:
+    if clay_positions[one_left]:
+      return (True, origin[0] - one_left[0])
+    one_left = (one_left[0]-1, one_left[1])
+    one_left_below = (one_left[0], one_left[1]+1)
+  return (False, origin[0] - one_left[0])
+
+def is_blocked_right(origin, clay_positions, settled_positions):
+  one_right = (origin[0]+1, origin[1])
+  one_right_below = (one_right[0], one_right[1]+1)
+  while clay_positions[one_right_below] or settled_positions[one_right_below]:
+    if clay_positions[one_right]:
+      return (True, one_right[0] - origin[0])
+    one_right = (one_right[0]+1, one_right[1])
+    one_right_below = (one_right[0], one_right[1]+1)
+  return (False, one_right[0] - origin[0])
+
+def print_map(clay, flowing, still, x1=300, x2=700, y1=0, y2=1000):
+  def char(p):
+    if p == SPRING:
+      return '+'
+    elif p in clay:
+      return '#'
+    elif p in both:
+      return '~' # For water that is tagged as both flowing and still
+    elif p in still:
+      return '~'
+    elif p in flowing:
+      return '|'
+    else:
+      return '.'
+
+  both = flowing & still
+  print('\n'.join(''.join(char(Point(x, y)) for x in range(x1, 1 + x2)) for y in range(y1, 1 + y2)))
+
 def main():
-  print("Hello World!")
+  clay_input = [l.rstrip() for l in open('input.txt', 'r').readlines()]
+  clay_positions = parse_clay(clay_input)
+  
+  water_positions = defaultdict(int)
+  settled_positions = defaultdict(int)
+  current_water_flowing_positions = {(500, 0)}
+  max_y = max(clay_positions.keys(), key=lambda x: x[1])[1]
+  print(max_y)
+  min_y = min(clay_positions.keys(), key=lambda x: x[1])[1]
+
+  while max(current_water_flowing_positions, key=lambda x: x[1])[1] < max_y:
+    new_positions = set()
+
+    for pos in current_water_flowing_positions:
+      one_below = (pos[0], pos[1]+1)
+
+      if not clay_positions[one_below] and not settled_positions[one_below]:
+        new_positions.add(one_below)
+        water_positions[one_below] = 1
+        continue
+
+      blocked_left_amount = is_blocked_left(pos, clay_positions, settled_positions)
+      blocked_right_amount = is_blocked_right(pos, clay_positions, settled_positions)
+      if blocked_left_amount[0] and blocked_right_amount[0]:
+        for x in range(pos[0]-(blocked_left_amount[1]-1), pos[0]+1):
+          settled_positions[(x, pos[1])] = 1
+        for x in range(pos[0]+1, pos[0]+blocked_right_amount[1]):
+          settled_positions[(x, pos[1])] = 1
+        one_above = (pos[0], pos[1]-1)
+        
+        new_positions.add(one_above)
+        continue
+
+      for x in range(pos[0]-(blocked_left_amount[1]-1), pos[0]+1):
+        water_positions[(x, pos[1])] = 1
+      if not blocked_left_amount[0]:
+        water_positions[(pos[0]-blocked_left_amount[1], pos[1])] = 1
+        new_positions.add((pos[0]-blocked_left_amount[1], pos[1]))
+      for x in range(pos[0]+1, pos[0]+blocked_right_amount[1]+1):
+        water_positions[(x, pos[1])] = 1
+      if not blocked_right_amount[0]:
+        new_positions.add((pos[0]+blocked_right_amount[1], pos[1]))
+    
+    current_water_flowing_positions = new_positions
+
+  flowing_coords = sorted(
+    [coord for coord, active in water_positions.items() if active],
+    key=lambda c: (c[1], c[0])
+  )
+
+  settled_coords = sorted(
+    [coord for coord, active in settled_positions.items() if active],
+    key=lambda c: (c[1], c[0])
+  )
+
+  clay_coords = [coord for coord, active in clay_positions.items() if active]
+
+  print_map(set(clay_coords), set(flowing_coords), set(settled_coords), y1=min_y, y2=max_y)
+
 
 if __name__ == '__main__':
   main()
