054331d481a0586729c0cc6ac1c20317f43a2118
Chris Williamson <chris.williamson@bbc.co.uk>
2018-12-20 09:00:18 +0000

    Almost finished day 19, got the solution manually.

---
+++ day19/functions.py
@@ -0,0 +1,171 @@
+def addr(register_states, a, b, output_register):
+    register_states = register_states.copy()
+    a_value = register_states[a]
+    b_value = register_states[b]
+
+    result = a_value + b_value
+    register_states[output_register] = result
+    return register_states
+
+def addi(register_states, a, b, output_register):
+    register_states = register_states.copy()
+    # print('RS: {0}'.format(register_states))
+    a_value = register_states[a]
+    b_value = b
+    # print(a_value, b_value)
+    result = a_value + b_value
+    register_states[output_register] = result
+    # print(register_states)
+    return register_states
+
+def mulr(register_states, a, b, output_register):
+    register_states = register_states.copy()
+    a_value = register_states[a]
+    b_value = register_states[b]
+    # print(register_states, a_value, b_value)
+    result = a_value * b_value
+    # print(result)
+    register_states[output_register] = result
+    return register_states
+
+def muli(register_states, a, b, output_register):
+    register_states = register_states.copy()
+    a_value = register_states[a]
+    b_value = b
+
+    result = a_value * b_value
+    register_states[output_register] = result
+    return register_states
+
+def banr(register_states, a, b, output_register):
+    register_states = register_states.copy()
+    a_value = register_states[a]
+    b_value = register_states[b]
+
+    result = int(bin(a_value & b_value), 2)
+    register_states[output_register] = result
+    return register_states
+
+def bani(register_states, a, b, output_register):
+    register_states = register_states.copy()
+    a_value = register_states[a]
+    b_value = b
+
+    result = int(bin(a_value & b_value), 2)
+    register_states[output_register] = result
+    return register_states
+
+def borr(register_states, a, b, output_register):
+    register_states = register_states.copy()
+    a_value = register_states[a]
+    b_value = register_states[b]
+
+    result = int(bin(a_value | b_value), 2)
+    register_states[output_register] = result
+    return register_states
+
+def bori(register_states, a, b, output_register):
+    register_states = register_states.copy()
+    a_value = register_states[a]
+    b_value = b
+
+    result = int(bin(a_value | b_value), 2)
+    register_states[output_register] = result
+    return register_states
+
+def setr(register_states, a, b, output_register):
+  register_states = register_states.copy()
+  a_value = register_states[a]
+  register_states[output_register] = a_value
+  return register_states
+
+def seti(register_states, a, b, output_register):
+  register_states = register_states.copy()
+  a_value = a
+  register_states[output_register] = a_value
+  return register_states
+
+def gtir(register_states, a, b, output_register):
+  register_states = register_states.copy()
+  a_value = a
+  b_value = register_states[b]
+
+  if a_value > b_value:
+    register_states[output_register] = 1
+  else:
+    register_states[output_register] = 0
+  return register_states
+
+def grti(register_states, a, b, output_register):
+  register_states = register_states.copy()
+  a_value = register_states[a]
+  b_value = b
+
+  if a_value > b_value:
+    register_states[output_register] = 1
+  else:
+    register_states[output_register] = 0
+  return register_states
+
+def gtrr(register_states, a, b, output_register):
+  register_states = register_states.copy()
+  a_value = register_states[a]
+  b_value = register_states[b]
+
+  if a_value > b_value:
+    register_states[output_register] = 1
+  else:
+    register_states[output_register] = 0
+  return register_states
+
+def eqir(register_states, a, b, output_register):
+  register_states = register_states.copy()
+  a_value = a
+  b_value = register_states[b]
+
+  if a_value == b_value:
+    register_states[output_register] = 1
+  else:
+    register_states[output_register] = 0
+  return register_states
+
+def eqri(register_states, a, b, output_register):
+  register_states = register_states.copy()
+  a_value = register_states[a]
+  b_value = b
+
+  if a_value == b_value:
+    register_states[output_register] = 1
+  else:
+    register_states[output_register] = 0
+  return register_states
+
+def eqrr(register_states, a, b, output_register):
+  register_states = register_states.copy()
+  a_value = register_states[a]
+  b_value = register_states[b]
+
+  if a_value == b_value:
+    register_states[output_register] = 1
+  else:
+    register_states[output_register] = 0
+  return register_states
+
+all_instruction_functions = [
+  addr,
+  addi,
+  mulr,
+  muli,
+  banr,
+  bani,
+  borr,
+  bori,
+  setr,
+  seti,
+  gtir,
+  grti,
+  gtrr,
+  eqir,
+  eqri,
+  eqrr
+]
--- day19/main.py
+++ day19/main.py
@@ -1,5 +1,101 @@
+from collections import defaultdict
+from functions import *
+
+def parse_input(input_lines):
+    ip = int(input_lines[0].split()[1])
+    instructions = defaultdict(dict)
+    for idx, i in enumerate(input_lines[1:]):
+        i_split = i.split()
+        instructions[idx] = {
+            'operation': i_split[0],
+            'A': int(i_split[1]),
+            'B': int(i_split[2]),
+            'output_reg': int(i_split[3])
+        }
+    return ip, instructions
+
 def main():
-  print("Hello World!")
+    instruction_pointer_register, instructions = parse_input([l.rstrip() for l in open('input.txt', 'r').readlines()])
+    # registers = [1, 0, 10551287, 3, 3, 10551287]
+    # registers = [128, 0, 10551287, 127, 3, 10551287]
+    registers = [10708661, 0, 10551287, 10551287, 3, 10551287]
+    instruction_pointer_value = 3
+
+    # while registers[3] < registers[2]:
+    #     if registers[3] % 1000000 == 0:
+    #         print(registers)
+    #     if registers[2] % registers[3] != 0 or (registers[2] % registers[3] == 0 and registers[0] > registers[3]):
+    #         registers[5] = registers[2]
+    #     elif registers[0] < registers[3]:
+    #         registers[5] = int(registers[2] / registers[3])
+    #     # else:
+    #     #     registers[5] = registers[2]
+
+    #     # state before: r1 will be r3 * r5-1
+
+    #     registers[1] = registers[3] * registers[5]
+    #     if registers[1] == registers[2]:
+    #         registers[1] = 1
+    #         # inst 7
+    #         registers[0] = registers[3] + registers[0]
+    #     else:
+    #         registers[1] = 0
+    #     registers[5] += 1
+    #     if registers[5] > registers[2]:
+    #         registers[1] = 1
+    #         registers[3] += 1
+    #         registers[5] = 1
+    #     else:
+    #         registers[1] = 0
+    #     # print(registers, end="\r")
+    #     # print(registers)
+    # print(registers)
+
+
+    while -1 < instruction_pointer_value < len(instructions):
+        current_instruction = instructions[instruction_pointer_value]
+        # print(current_instruction)
+        old_registers = registers
+        registers[instruction_pointer_register] = instruction_pointer_value
+        new_registers = eval('{0}({1}, {2}, {3}, {4})'.format(
+            current_instruction['operation'],
+            registers,
+            current_instruction['A'],
+            current_instruction['B'],
+            current_instruction['output_reg'])
+        )
+        new_instruction_pointer_value = new_registers[instruction_pointer_register] + 1
+        print('ip={0} {1} {2} {3} {4} {5} {6}'.format(
+            instruction_pointer_value,
+            old_registers
+            ,
+            current_instruction['operation'],
+            current_instruction['A'],
+            current_instruction['B'],
+            current_instruction['output_reg'],
+            new_registers
+        ))
+        registers = new_registers
+        instruction_pointer_value = new_instruction_pointer_value
+        # print(instruction_pointer_value)
+        # print(registers)
+        
+    print(registers)
+
+
+# def inner_loop():
+#     while r1 < r2:
+#         r4 = r1 + r4
+#         r0 = r3 + r0
+#         r5 = r5 + 1
+#         if r5 > r2:
+#             r1 = 1
+#         else:
+#             r1 = 0
+#         r4 = r4 + r1
+
+
+
 
 if __name__ == '__main__':
-  main()
+    main()
